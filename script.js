c5e97eb12313cb611361b3d872060bab9ca2bd35
ugbeadie <ugbeadie3@gmail.com>
2022-05-19 07:54:20 +0100

    shortened code

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const btn = document.getElementsByClassName('btn')[0];
 
 function getAdvice() {
     fetch('https://api.adviceslip.com/advice')
-    .then (response =>{
-    return response.json();
-})
-    .then(adviceData => {
-        const adviceNumber = adviceData.slip.id;
-        const advice = adviceData.slip.advice;
+        .then(response => {
+            return response.json();
+        })
+        .then(adviceData => {
+            const adviceNumber = adviceData.slip.id;
+            const advice = adviceData.slip.advice;
 
-        adviceId.textContent = adviceNumber;
-        adviceText.innerHTML = `<p>${advice}</p>`;
-    })
-    .catch(error => {
-        console.log(error);
-    })
+            adviceId.textContent = adviceNumber;
+            adviceText.innerHTML = advice;
+        })
+        .catch(error => {
+            console.log(error);
+        })
 }
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     getAdvice();
 })
 
